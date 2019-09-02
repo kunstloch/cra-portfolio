21e25ebc3bb8e71983256d99aad248dc75724a0c
Alexander Scherer <alexscherer@outlook.com>
2019-09-02 15:23:08 +0200

    change name and background pic

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,7 @@ import React from 'react';
 import profilePic from './profilepic.jpg';
 
 let data = {
-  name: 'Rahul Bhatia',
+  name: 'Alexander Scherer',
   aboutme:
     'I am currently a pre-final year student at The LNM Institute of Information Technology and pursuing my B.Tech from here. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.',
   address: 'India',
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 let data = {
-  name: 'Rahul Bhatia',
+  name: 'Alexander Scherer',
   role: 'Frontend Developer and Data Scientist',
   roleDescription:
     'I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.',
