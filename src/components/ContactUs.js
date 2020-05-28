7457a64de2c4e29c9e39f89a40bc7382a8937fc9
Alexander Scherer <alexscherer@outlook.com>
2020-05-28 17:54:11 +0200

    Change Mailaddress

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -1,12 +1,18 @@
 import React from 'react';
 
 export default function ContactUs() {
+  const part1 = "alexscherer";
+  const part2 = Math.pow(2,6);
+  const part3 = String.fromCharCode(part2);
+  const part4 = "outlook.com";
+  const part5 = part1 + String.fromCharCode(part2) + part4;
+  const part6 = "mailto:";
+
   return (
     <section id="contact">
       <p className="impressum">
         Impressum: Alexander Scherer-Sokolowski | Tel: +43 664 7501 8228 <br />
-        Schulstrasse 1 - 7304 Grosswarasdorf - Österreich <br />
-        Mail: alexscherer@outlook.com
+        Schulstrasse 1 - 7304 Grosswarasdorf - Österreich <br />email:  <a href={part6 + part5}>{ part5 }</a>
       </p>
     </section>
   );
